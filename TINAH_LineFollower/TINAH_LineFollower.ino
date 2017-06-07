654bedae5a385223967b0a3de5ec454d308c5ee8
KeenanMcConkey <k.t.mcconkey@gmail.com>
2017-06-07 16:58:43 -0700

    Update LineFollower code

--- TINAH_LineFollower/TINAH_LineFollower.ino
+++ TINAH_LineFollower/TINAH_LineFollower.ino
@@ -15,21 +15,21 @@
 
 #define FWD_OFFSET 20
 #define BWD_OFFSET 20
-#define TURN_OFFSET_LOW_SLOW 60 // For turning at low speed
+#define TURN_OFFSET_LOW_SLOW 60
 #define TURN_OFFSET_HIGH_SLOW 40
-#define TURN_OFFSET_LOW_FAST 40 // For turning at high speed
+#define TURN_OFFSET_LOW_FAST 40
 #define TURN_OFFSET_HIGH_FAST 20
 
-#define HIGH_THRESHOLD 75
-#define LOW_THRESHOLD 300
+#define HIGH_THRESHOLD 300
+#define LOW_THRESHOLD 50
 
 #define LOW_LIGHT 0
 #define MED_LIGHT 1
 #define HIGH_LIGHT 2
 
-#define REALIGNMENT_DELAY 200
+#define BWD_RA_DELAY 100
+#define TURN_RA_DELAY 100
 
-// Global variables to keep track of current (most recent) L and R motor speeds
 int currentRightSpeed = 0;
 int currentLeftSpeed = 0;
 
@@ -39,23 +39,19 @@ void setup() {
   
   LCD.clear(); LCD.home();
   LCD.setCursor(0,0); LCD.print("Press START");
-  while(! (startbutton())); // Do nothing
+  while(! (startbutton()));
 }
 
 void loop() {
-  // Get analog input from light sensors (digital inputs) 
   int analog_R = analogRead(TMP_PIN_R);
   int analog_L = analogRead(TMP_PIN_L);
 
-  // Show analog outputs of sensors on the SERIAL.
-  Serial.print("R - "); Serial.println(analog_R);
-  Serial.print("L - "); Serial.println(analog_L);
+  Serial.print("TMP_R = "); Serial.println(analog_R);
+  Serial.print("TMP_L = "); Serial.println(analog_L);
 
-  // Compare values to thresholds
   int tmp_R = compareThreshold(analog_R);
   int tmp_L = compareThreshold(analog_L);
 
-  // Move a direction based off readings
   moveDirection(tmp_R, tmp_L);
 }
 
@@ -84,7 +80,7 @@ void moveDirection(int tmp_R, int tmp_L) {
   else if (tmp_R == HIGH_LIGHT && tmp_L == HIGH_LIGHT) 
     moveBacktrack();
   else
-    moveBrake();
+    moveBrake(); // ERROR CASE
 }
 
 void moveForward() {
@@ -143,48 +139,25 @@ void moveRightFast() {
 }
 
 void moveBacktrack() {
-  motor.speed(MOTOR_R, BRAKE_SPEED);
-  motor.speed(MOTOR_L, BRAKE_SPEED);
-  delay(REALIGNMENT_DELAY);
-
   int speedDiff = currentRightSpeed - currentLeftSpeed;
-
-  // Right speed > Left speed condition (moving left direction)
-  /*if (speedDiff > 0) {
-    motor.speed(MOTOR_R, MAX_BWD_SPEED + TURN_OFFSET_LOW_SLOW);
-    motor.speed(MOTOR_L, BRAKE_SPEED);
-
-    // Track current speeds
-    currentRightSpeed = MAX_BWD_SPEED + TURN_OFFSET_LOW_SLOW; 
-    currentLeftSpeed = BRAKE_SPEED;
-
-  // Left speed > Right speed condition (moving right direction)
-  } else if (speedDiff < 0) {
-    motor.speed(MOTOR_R, BRAKE_SPEED);
-    motor.speed(MOTOR_L, MAX_BWD_SPEED + TURN_OFFSET_LOW_SLOW);
-
-    // Track current speeds
-    currentRightSpeed = BRAKE_SPEED; 
-    currentLeftSpeed = MAX_BWD_SPEED + TURN_OFFSET_LOW_SLOW;
   
-  // If both speeds are equal, reverses motors to try and backtrack to where tape was
-  } else { */
-    // Set speed to 0 to allow realignment process to begin
-    motor.speed(MOTOR_R, BRAKE_SPEED);
-    motor.speed(MOTOR_L, BRAKE_SPEED);
-    delay(100); // Short delay for controlled realignment process
-
-    // Short BWD reverse pulse
-    motor.speed(MOTOR_R, MAX_BWD_SPEED);
-    motor.speed(MOTOR_L, MAX_BWD_SPEED);
-    delay(20); // Short delay for controlled realignment process
-    
+  if (speedDiff > 0) // R > L
+    moveLeftFast();
+    delay(TURN_RA_DELAY);
+  else if (speedDiff < 0) { // L > R
+    moveRightFast();
+    delay(TURN_RA_DELAY);
+  else {
     currentRightSpeed = MAX_BWD_SPEED + BWD_OFFSET; 
-    currentLeftSpeed = MAX_BWD_SPEED + BWD_OFFSET;
-  //}
+    currentLeftSpeed  = MAX_BWD_SPEED + BWD_OFFSET;
+    
+    motor.speed(MOTOR_R, currentRightSpeed);
+    motor.speed(MOTOR_L, currentLeftSpeed);
+    delay(BWD_RA_DELAY);
+  }
 
   LCD.clear(); LCD.home();
-  LCD.setCursor(0,0); LCD.print("REALIGNING");
+  LCD.setCursor(0,0); LCD.print("REALIGN");
 }
 
 void moveBrake() {
@@ -192,6 +165,6 @@ void moveBrake() {
   motor.speed(MOTOR_L, BRAKE_SPEED);
 
   LCD.clear(); LCD.home();
-  LCD.setCursor(0,0); LCD.print("BRAKE");
+  LCD.setCursor(0,0); LCD.print("ERROR - BRAKE");
 }
 
